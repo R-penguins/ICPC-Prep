588595f2601b8131d42335f0dabe05a8bdbb722a
Rui Hou <harryhourui@outlook.com>
2021-02-26 12:00:40 +0800

    update

--- starter.cpp
+++ starter.cpp
@@ -1,5 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
+using VD = vector<double>;
+using VVD = vector<VD>;
+using VI = vector<int>;
+using VII = vector<VI>;
+constexpr double EPS = 1e-9;
 
 int main(int argc, char const *argv[])
 {
